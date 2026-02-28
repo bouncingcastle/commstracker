692096ec3e1a4db9b8592a470d8c0bc054c351cb
System Administrator <admin@example.com>
2026-02-28 15:38:17 -0800

    commit

---
+++ src/fluent/generated/data/form-section/sys_ui_section_1868e3d683d73210a0365dc6feaad38d.now.ts
@@ -0,0 +1,14 @@
+import { Record } from '@servicenow/sdk/core'
+
+Record({
+    $id: Now.ID['1868e3d683d73210a0365dc6feaad38d'],
+    table: 'sys_ui_section',
+    data: {
+        header: false,
+        name: 'x_823178_commissio_commission_plans',
+        sys_domain: 'global',
+        sys_domain_path: '/',
+        title: true,
+        view: 'Default view',
+    },
+})
---
+++ src/fluent/generated/other/sys-ui-element/sys_ui_section_1868e3d683d73210a0365dc6feaad38d.now.ts
@@ -0,0 +1,227 @@
+import { Record } from '@servicenow/sdk/core'
+
+Record({
+    $id: Now.ID['dc6827d683d73210a0365dc6feaad3f6'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.begin_split',
+        position: 0,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+        type: '.begin_split',
+    },
+})
+Record({
+    $id: Now.ID['946827d683d73210a0365dc6feaad3f8'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'is_active',
+        position: 1,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['506827d683d73210a0365dc6feaad3f9'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.split',
+        position: 2,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+        type: '.split',
+    },
+})
+Record({
+    $id: Now.ID['1c6827d683d73210a0365dc6feaad3f9'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'upsell_rate',
+        position: 3,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['d46827d683d73210a0365dc6feaad3fa'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.end_split',
+        position: 4,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+        type: '.end_split',
+    },
+})
+Record({
+    $id: Now.ID['906827d683d73210a0365dc6feaad3fb'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'plan_name',
+        position: 5,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['5c6827d683d73210a0365dc6feaad3fb'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.begin_split',
+        position: 6,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+        type: '.begin_split',
+    },
+})
+Record({
+    $id: Now.ID['186827d683d73210a0365dc6feaad3fc'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'sales_rep',
+        position: 7,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['d06827d683d73210a0365dc6feaad3fd'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'new_business_rate',
+        position: 8,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['9c6827d683d73210a0365dc6feaad3fd'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'base_rate',
+        position: 9,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['586827d683d73210a0365dc6feaad3fe'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.split',
+        position: 10,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+        type: '.split',
+    },
+})
+Record({
+    $id: Now.ID['146827d683d73210a0365dc6feaad3ff'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'effective_start_date',
+        position: 11,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['dc6827d683d73210a0365dc6feaad3ff'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'renewal_rate',
+        position: 12,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['986867d683d73210a0365dc6feaad300'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.end_split',
+        position: 13,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+        type: '.end_split',
+    },
+})
+Record({
+    $id: Now.ID['546867d683d73210a0365dc6feaad301'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'description',
+        position: 14,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['106867d683d73210a0365dc6feaad302'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.begin_split',
+        position: 15,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+        type: '.begin_split',
+    },
+})
+Record({
+    $id: Now.ID['d86867d683d73210a0365dc6feaad302'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'effective_end_date',
+        position: 16,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['946867d683d73210a0365dc6feaad303'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'expansion_rate',
+        position: 17,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['506867d683d73210a0365dc6feaad304'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'deal_type',
+        position: 18,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['1c6867d683d73210a0365dc6feaad304'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.split',
+        position: 19,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+        type: '.split',
+    },
+})
+Record({
+    $id: Now.ID['d46867d683d73210a0365dc6feaad305'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'plan_overlap_approved_by',
+        position: 20,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['906867d683d73210a0365dc6feaad306'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'plan_overlap_approved_on',
+        position: 21,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
+Record({
+    $id: Now.ID['5c6867d683d73210a0365dc6feaad306'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.end_split',
+        position: 22,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+        type: '.end_split',
+    },
+})
+Record({
+    $id: Now.ID['186867d683d73210a0365dc6feaad307'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'plan_overlap_reason',
+        position: 23,
+        sys_ui_section: '1868e3d683d73210a0365dc6feaad38d',
+    },
+})
