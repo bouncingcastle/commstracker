29a435633087e5245d391e1074d4b61b1a56d2d4
System Administrator <admin@example.com>
2026-02-28 14:43:39 -0800

    commit

---
+++ src/fluent/generated/other/sys-scope-privilege/sys_scope_privilege_e74a0f1a83d33210a0365dc6feaad3fb.now.ts
@@ -0,0 +1,10 @@
+import { CrossScopePrivilege } from '@servicenow/sdk/core'
+
+CrossScopePrivilege({
+    $id: Now.ID['e74a0f1a83d33210a0365dc6feaad3fb'],
+    operation: 'read',
+    status: 'allowed',
+    targetName: 'sys_user',
+    targetScope: 'global',
+    targetType: 'sys_db_object',
+})
